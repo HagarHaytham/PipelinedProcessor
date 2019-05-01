ab174ccf84ed4f722e27552d7a09ac64d9ce6526
Hagar Haytham <hagar.bahy97@eng-st.cu.edu.eg>
2019-05-01 21:32:10 +0200

    flag bug fixed

--- Do Files/ALU.do
+++ Do Files/ALU.do
@@ -1,9 +1,33 @@
+vsim -gui work.alu
 add wave -position insertpoint sim:/alu/*
-force -freeze sim:/alu/s 00000 0
-force -freeze sim:/alu/flagsIn 000 0
-run
+# NOP
 force -freeze sim:/alu/b 00000000000000 0
 force -freeze sim:/alu/a 0000000000000000 0
 force -freeze sim:/alu/s 00000 0
 force -freeze sim:/alu/flagsIn 000 0
 run
+
+#SHR 
+force -freeze sim:/alu/a 0000000000000001 0
+force -freeze sim:/alu/b 0000000000000001 0
+force -freeze sim:/alu/s 01110 0
+run
+
+#SHR a =64 c = 0C 
+force -freeze sim:/alu/a 0000000001100100 0
+force -freeze sim:/alu/b 0000000000000011 0
+force -freeze sim:/alu/s 01110 0
+run
+
+#SHL a=19 C--> 0, N -->0 , Z -->0
+force -freeze sim:/alu/a 0000000000011001 0
+force -freeze sim:/alu/b 0000000000000010 0
+force -freeze sim:/alu/s 01101 0
+run
+
+#SHL 
+force -freeze sim:/alu/a 1000000000000000 0
+force -freeze sim:/alu/b 0000000000000001 0
+force -freeze sim:/alu/s 01101 0
+run
+
--- VHDL/TwoOperandALU.vhd
+++ VHDL/TwoOperandALU.vhd
@@ -1,12 +1,13 @@
 LIBRARY IEEE;
 USE IEEE.std_logic_1164.all;
 USE IEEE.numeric_std.all;
+use ieee.std_logic_unsigned.all; 
 ENTITY TwoOperandALU IS
 	GENERIC (m:integer:=16);
 	PORT(a,b: IN STD_LOGIC_VECTOR(m-1 DOWNTO 0); --a-> Rsrc , b-> Rdst (put imm in it in shift operations)
         s : IN STD_LOGIC_VECTOR(2 DOWNTO 0);
         flagsIn : IN STD_LOGIC_VECTOR(2 DOWNTO 0);
-        c : OUT STD_LOGIC_VECTOR(m-1 DOWNTO 0);
+		c : OUT STD_LOGIC_VECTOR(m-1 DOWNTO 0);
         flagsOut: OUT STD_LOGIC_VECTOR(2 DOWNTO 0)); -- C - N - Z
 END ENTITY TwoOperandALU;
 
@@ -32,6 +33,7 @@ COMPONENT FullAdder IS
 		cout : OUT STD_LOGIC );
 END COMPONENT FullAdder;
 SIGNAL adderin2,bbar,ad,an,o,sl1,sr1,myout : STD_LOGIC_VECTOR(m-1 DOWNTO 0);
+SIGNAL expandA ,expandA2: STD_LOGIC_VECTOR(m DOWNTO 0);
 Signal ccrAdd,ccrAnd,ccrShl,ccrShr : STD_LOGIC_VECTOR (2 downto 0);
 SIGNAL carrin : STD_LOGIC_VECTOR(0 DOWNTO 0);
 SIGNAL carrout,z : STD_LOGIC;
@@ -41,23 +43,20 @@ an <= a AND b;
 o<= a OR b;
 sl1 <=std_logic_vector(unsigned(a) sll to_integer(unsigned(b(3 DOWNTO 0))));
 sr1 <=std_logic_vector(unsigned(a) srl to_integer(unsigned(b(3 DOWNTO 0))));
---sl1<= (m-1 DOWNTO to_integer(unsigned(b(3 DOWNTO 0)))=>a(m-1-to_integer(unsigned(b(3 DOWNTO 0))) DOWNTO 0), others =>'0');
---sr1<= (m-1 DOWNTO to_integer(unsigned(b(3 DOWNTO 0)))=>a(m-1-to_integer(unsigned(b(3 DOWNTO 0))) DOWNTO 0), others =>'0');
-
 z <= '1' when myout=x"0000"
 else '0';
 ccrAdd <= carrout & myout(15) &z;
 ccrAnd <= flagsIn(2) & myout(15) &z;
---ccrShl <= a(16-to_integer(unsigned(b)))& myout(15) &z;  -- need to adjust carry
---ccrShr <= a(to_integer(unsigned(b)))& myout(15) &z;  -- need to adjust carry
+expandA <= a&'0';
+expandA2 <= '0'&a;
+ccrShl <=  expandA2(16-(to_integer(unsigned(b(3 DOWNTO 0)))))& myout(15) &z;  -- need to adjust carry
+ccrShr <= expandA((to_integer(unsigned(b(3 DOWNTO 0)))))& myout(15) &z;  -- need to adjust carry
 Mb : MUX2x1 GENERIC MAP (m) PORT MAP (b,bbar,s(1),adderin2);
 Mc : MUX2x1 GENERIC MAP (1) PORT MAP ("0","1",s(1),carrin);
 FA : FullAdder GENERIC MAP (m) PORT MAP (a,adderin2,carrin(0),ad,carrout);
 M1 : MUX8x1 GENERIC MAP (m)  PORT MAP (a,ad,ad,an,o,sl1,sr1,x"0000",s,myout);
-M2 : MUX8x1 GENERIC MAP (3) PORT MAP (flagsIn,ccrAdd,ccrAdd,ccrAnd,ccrAnd,ccrAnd,ccrAnd,flagsIn,s,flagsOut);
+M2 : MUX8x1 GENERIC MAP (3) PORT MAP (flagsIn,ccrAdd,ccrAdd,ccrAnd,ccrAnd,ccrShl,ccrShr,flagsIn,s,flagsOut);
 c<= myout;
-
--- flagsOut <="000";
 END ARCHITECTURE;
 
 
